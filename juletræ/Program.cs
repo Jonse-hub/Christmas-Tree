c4c8538ea182cd6401f7b8d50e543d8fd5f62b46
Jonse-hub <jona444j@edu.campusvejle.dk>
2023-11-10 14:26:05 +0100

    Yup

---
+++ juletræ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace juletræ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            juletræ(10);
+        }
+
+        private static void juletræ(int højde)
+        {
+
+            for (int i = 0; i < højde; i++)
+            {
+                for (int c = højde - i; c > 0; c--)
+                {
+                    Console.Write(" ");
+                }
+                for (int b = 0; b < i*2; b++)
+                {
+                    
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
